3534e07dd26021bc1ed050a221259697e14df03f
Stasia Soloduha <teoretic.nest@gmail.com>
2022-09-07 13:48:23 +0300

    New shiny bot

---
+++ bot.py
@@ -0,0 +1,33 @@
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+import Setting
+
+logging.basicConfig(filename = 'bot.log', level = logging.INFO)
+
+
+
+def greet_user(update, context):
+    print('Вызов /старт')
+    #print(update)
+    update.message.reply_text('Здравствуй, пользователь.')
+
+def talk_to_me(update, context):
+    text = update.message.text
+    print(text)
+    update.message.reply_text(text)
+
+def main():
+    mybot = Updater(Setting.API_KEY, use_context=True) 
+
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler('start',greet_user))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+
+    logging.info('Бот стартовал')
+    mybot.start_polling()
+    mybot.idle()
+
+if __name__ = '__main__':
+    main()
+ 
